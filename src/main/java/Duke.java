dabe62177ecc1cac7eca94f7c665332608d2e685
Yonish18 <151393231+Yonish18@users.noreply.github.com>
2026-02-04 06:54:43 +0800

    Implement echo() function and update greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,33 @@
-public class Duke {
-    public static void main(String[] args) {
-        String welcomeMsg = "____________________________________________________________\n" +
-                " Hello! I'm Chotu\n" +
-                " What can I do for you?" ;
+import java.util.Scanner;
 
-        String exitMsg =                 "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________" ;
+public class Duke {
+
+    private static String divider = "\u001B[34m" + "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n";
+    public static void main(String[] args) {
+        String welcomeMsg = divider +
+                " >> CHOTU ACTIVATED <<\n" +
+                " Namaste! I'm Chotu\n" +
+                " Your wish is my command. What can I do for you?" ;
+        String exitMsg =                 divider +
+                "Bye-Bye. Have a good day! Jai Shree Ram!\n" +
+                divider ;
+
 
         System.out.println(welcomeMsg);
+        echo();
         System.out.println(exitMsg);
     }
 
+    public static void echo() {
+        String userInput = takeUserInput();
+        while (!(userInput.toLowerCase().equals("bye"))) {
+            System.out.println(divider + userInput + "\n" + divider);
+            userInput = takeUserInput();
+        }
+    }
+
+    public static String takeUserInput() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
 }
